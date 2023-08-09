053cd76d96c2d4d137f9320748f925aa0e06b3ac
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-09 09:03:45 +0530

    Create HomeController.php

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\User;
+
+class HomeController extends Controller
+{
+    public function redirect()
+    {
+        $usertype=Auth::user()->usertype;
+
+        if($usertype=='1')
+        {
+            return view('admin.home');
+        }
+
+        else
+        {
+            return view('dashboard');
+        }
+    }
+}
