31dec0131f33d06e6d2196a5f0f0e50266f28cca
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-10 04:50:01 +0530

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -21,7 +21,12 @@ class HomeController extends Controller
 
         else
         {
-            return view('dashboard');
+            return view('user.home');
         }
+    }
+
+    public function index()
+    {
+        return view('user.home');
     }
 }
