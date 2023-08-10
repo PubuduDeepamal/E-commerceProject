5d165428a28b26ea3ab8f83024eae7ba436a5228
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-11 04:18:07 +0530

    Update product.blade.php

--- resources/views/user/product.blade.php
+++ resources/views/user/product.blade.php
@@ -9,27 +9,22 @@
         </div>
 
 
+        @foreach ($data as $product)
+
         <div class="col-md-4">
           <div class="product-item">
             <a href="#"><img src="assets/images/product_06.jpg" alt=""></a>
             <div class="down-content">
-              <a href="#"><h4>Tittle goes here</h4></a>
-              <h6>$22.50</h6>
-              <p>Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
-              <ul class="stars">
-                <li><i class="fa fa-star"></i></li>
-                <li><i class="fa fa-star"></i></li>
-                <li><i class="fa fa-star"></i></li>
-                <li><i class="fa fa-star"></i></li>
-                <li><i class="fa fa-star"></i></li>
-              </ul>
-              <span>Reviews (32)</span>
+              <a href="#"><h4>{{ $product->title }}</h4></a>
+              <h6>${{ $product->price }}</h6>
+              <p>{{ $product->description }}</p>
+              
             </div>
           </div>
         </div>
 
+        @endforeach
 
-        
       </div>
     </div>
   </div>
