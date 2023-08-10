f8e4308dca8fdea4a0a8f4f70c3d344a5715ba58
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-10 13:49:39 +0530

    Update product.blade.php

--- resources/views/admin/product.blade.php
+++ resources/views/admin/product.blade.php
@@ -2,10 +2,26 @@
 <html lang="en">
   <head>
       @include('admin.css')
+
+      <style>
+
+        .title{
+          color: white; 
+          padding-top: 25px;
+          font-size:25px;
+        }
+      </style>
   </head>
   <body>  
       @include('admin.sidebar')
       @include('admin.navbar')
+      <div class="container-fluid page-body-wrapper">
+        <div class="container" align="center">
+          <h1 class="title">Add Product</h1>
+        </div>
+
+        
+      </div>
       @include('admin.script')    
   </body>
 </html>
