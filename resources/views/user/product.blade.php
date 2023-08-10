3f1168cca0cb406ddf512ddd400978c4885b8fc5
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-11 04:11:29 +0530

    Update product.blade.php

--- resources/views/user/product.blade.php
+++ resources/views/user/product.blade.php
@@ -7,96 +7,8 @@
             <a href="products.html">view all products <i class="fa fa-angle-right"></i></a>
           </div>
         </div>
-        <div class="col-md-4">
-          <div class="product-item">
-            <a href="#"><img src="assets/images/product_01.jpg" alt=""></a>
-            <div class="down-content">
-              <a href="#"><h4>Tittle goes here</h4></a>
-              <h6>$25.75</h6>
-              <p>Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
-              <ul class="stars">
-                <li><i class="fa fa-star"></i></li>
-                <li><i class="fa fa-star"></i></li>
-                <li><i class="fa fa-star"></i></li>
-                <li><i class="fa fa-star"></i></li>
-                <li><i class="fa fa-star"></i></li>
-              </ul>
-              <span>Reviews (24)</span>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="product-item">
-            <a href="#"><img src="assets/images/product_02.jpg" alt=""></a>
-            <div class="down-content">
-              <a href="#"><h4>Tittle goes here</h4></a>
-              <h6>$30.25</h6>
-              <p>Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
-              <ul class="stars">
-                <li><i class="fa fa-star"></i></li>
-                <li><i class="fa fa-star"></i></li>
-                <li><i class="fa fa-star"></i></li>
-                <li><i class="fa fa-star"></i></li>
-                <li><i class="fa fa-star"></i></li>
-              </ul>
-              <span>Reviews (21)</span>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="product-item">
-            <a href="#"><img src="assets/images/product_03.jpg" alt=""></a>
-            <div class="down-content">
-              <a href="#"><h4>Tittle goes here</h4></a>
-              <h6>$20.45</h6>
-              <p>Sixteen Clothing is free CSS template provided by TemplateMo.</p>
-              <ul class="stars">
-                <li><i class="fa fa-star"></i></li>
-                <li><i class="fa fa-star"></i></li>
-                <li><i class="fa fa-star"></i></li>
-                <li><i class="fa fa-star"></i></li>
-                <li><i class="fa fa-star"></i></li>
-              </ul>
-              <span>Reviews (36)</span>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="product-item">
-            <a href="#"><img src="assets/images/product_04.jpg" alt=""></a>
-            <div class="down-content">
-              <a href="#"><h4>Tittle goes here</h4></a>
-              <h6>$15.25</h6>
-              <p>Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
-              <ul class="stars">
-                <li><i class="fa fa-star"></i></li>
-                <li><i class="fa fa-star"></i></li>
-                <li><i class="fa fa-star"></i></li>
-                <li><i class="fa fa-star"></i></li>
-                <li><i class="fa fa-star"></i></li>
-              </ul>
-              <span>Reviews (48)</span>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="product-item">
-            <a href="#"><img src="assets/images/product_05.jpg" alt=""></a>
-            <div class="down-content">
-              <a href="#"><h4>Tittle goes here</h4></a>
-              <h6>$12.50</h6>
-              <p>Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
-              <ul class="stars">
-                <li><i class="fa fa-star"></i></li>
-                <li><i class="fa fa-star"></i></li>
-                <li><i class="fa fa-star"></i></li>
-                <li><i class="fa fa-star"></i></li>
-                <li><i class="fa fa-star"></i></li>
-              </ul>
-              <span>Reviews (16)</span>
-            </div>
-          </div>
-        </div>
+
+
         <div class="col-md-4">
           <div class="product-item">
             <a href="#"><img src="assets/images/product_06.jpg" alt=""></a>
@@ -115,6 +27,9 @@
             </div>
           </div>
         </div>
+
+
+        
       </div>
     </div>
   </div>
