2811a5dd9bbfb184b61a81cb326a5055bed9195e
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-10 17:17:10 +0530

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,10 +4,38 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Product;
+
 class AdminController extends Controller
 {
     public function product()
    {
-    return view('admin.product');
+        return view('admin.product');
+   }
+
+   public function uploadproduct(Request $request)
+   {
+     $data = new product;
+
+     $image =$request->file;
+
+     $imagename =time().'.'.$image->getClientOriginalExtension();
+
+     $request->file->move('productimage',$imagename);
+
+     $data->image=$imagename;
+
+     $data->title=$request->title;
+
+     $data->price=$request->price;
+
+     $data->description=$request->des;
+
+     $data->quantity=$request->quantity;
+
+     $data->save();
+
+     return redirect()->back();
+     
    }
 }
