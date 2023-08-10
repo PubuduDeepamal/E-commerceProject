1f2732041d0916b7d187caffd6b45126c82189b6
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-10 13:49:32 +0530

    Update navbar.blade.php

--- resources/views/admin/navbar.blade.php
+++ resources/views/admin/navbar.blade.php
@@ -1,4 +1,4 @@
-<div class="container-fluid page-body-wrapper">
+
     <!-- partial:partials/_navbar.html -->
     <nav class="navbar p-0 fixed-top d-flex flex-row">
       <div class="navbar-brand-wrapper d-flex d-lg-none align-items-center justify-content-center">
