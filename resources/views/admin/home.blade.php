0183970ebc3b865f8e8be05db5c551812d01b039
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-10 09:03:49 +0530

    admin template

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,20 +1,3 @@
-{{-- <html>
-
-    <head>
-
-        <meta charset="utf-8">
-        <title></title>
-
-    </head>
-
-    <body>
-
-        <h1>Admin Dashboard</h1>
-
-    </body>
-
-</html> --}}
-
 <x-app-layout>
    
 </x-app-layout>
