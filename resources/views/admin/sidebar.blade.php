cdc71220c27372bfcfecb91084794abead70d971
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-11 09:08:11 +0530

    Update sidebar.blade.php

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -93,5 +93,15 @@
             <span class="menu-title">Add New Products</span>
           </a>
         </li>
+
+        <li class="nav-item menu-items">
+          <a class="nav-link" href="{{url('showproduct')}}">
+            <span class="menu-icon">
+              <i class="mdi mdi-file-document-box"></i>
+            </span>
+            <span class="menu-title">Show All Products</span>
+          </a>
+        </li>
+
       </ul>
     </nav>
