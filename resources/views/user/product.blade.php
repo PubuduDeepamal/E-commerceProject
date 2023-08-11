f50475dc1715ce05f9f759a65b143c50f3eecdee
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-11 15:11:39 +0530

    Update product.blade.php

--- resources/views/user/product.blade.php
+++ resources/views/user/product.blade.php
@@ -12,11 +12,11 @@
         @foreach ($data as $product)
 
         <div class="col-md-4">
-          <div class="product-item">
+          <div class="product-item" style="height: 520px">
             <a href="#"><img height="300" width="150" src="/productimage/{{$product->image}}" alt=""></a>
             <div class="down-content">
               <a href="#"><h4>{{ $product->title }}</h4></a>
-              <h6>${{ $product->price }}</h6>
+              <h6>Rs {{ $product->price }}</h6>
               <p>{{ $product->description }}</p>
               
             </div>
