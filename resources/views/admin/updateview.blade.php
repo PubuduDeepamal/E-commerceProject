ebcaae5433f8f4b68cf4113dfa9f0a57d363597e
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-11 12:40:59 +0530

    Create updateview.blade.php

---
+++ resources/views/admin/updateview.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+      @include('admin.css')
+  </head>
+  <body>  
+      @include('admin.sidebar')
+      @include('admin.navbar')
+      @include('admin.script')    
+  </body>
+</html>
