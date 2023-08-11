31a1ca005a5b5a97a6ab6330ced65249651ffdeb
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-11 09:17:05 +0530

    Update showproduct.blade.php

--- resources/views/admin/showproduct.blade.php
+++ resources/views/admin/showproduct.blade.php
@@ -6,6 +6,38 @@
   <body>  
       @include('admin.sidebar')
       @include('admin.navbar')
+
+
+      <div class="container-fluid page-body-wrapper">
+        <div class="container" align="center">
+
+            <table>
+
+                <tr style="background-color:gray">
+
+                    <td style="padding: 20px">Title </td>
+                    <td style="padding: 20px">Description </td>
+                    <td style="padding: 20px">Quntity </td>
+                    <td style="padding: 20px">Price </td>
+                    <td style="padding: 20px">Image </td>
+
+                </tr>
+
+                <tr style="background-color:black; align-items:center;">
+
+                    <td>Title </td>
+                    <td>Description </td>
+                    <td>Quntity </td>
+                    <td>Price </td>
+                    <td>Image </td>
+
+                </tr>
+
+            </table>
+
+        </div>
+
+      </div>
    
       @include('admin.script')    
   </body>
