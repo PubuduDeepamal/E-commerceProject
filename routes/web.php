a194a2e002f2d61db6c45206d9b735e000dc6d37
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-11 13:11:18 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -44,3 +44,5 @@ route::get('/showproduct',[AdminController::class,'showproduct']);
 route::get('/deleteproduct/{id}',[AdminController::class,'deleteproduct']);
 
 route::get('/updateview/{id}',[AdminController::class,'updateview']);
+
+route::post('/updateproduct/{id}',[AdminController::class,'updateproduct']);
