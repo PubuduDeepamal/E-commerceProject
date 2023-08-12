039d87f791c723bda2a3b2494810a20250722b3c
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-12 09:44:53 +0530

    Update heder.blade.php

--- resources/views/user/heder.blade.php
+++ resources/views/user/heder.blade.php
@@ -1,50 +1,50 @@
  <!-- Header -->
  <header class="">
-    <nav class="navbar navbar-expand-lg">
-      <div class="container">
-        <a class="navbar-brand" href="index.html"><h2 style="font-family: 'Times New Roman', Times, serif;">JACK <em>Pets</em></h2></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-              <a class="nav-link" style="font-family: 'Times New Roman', Times, serif;"  href="index.html">Home
-                <span class="sr-only">(current)</span>
-              </a>
-            </li> 
-            <li class="nav-item">
-              <a class="nav-link" style="font-family: 'Times New Roman', Times, serif;" href="products.html">Our Products</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" style="font-family: 'Times New Roman', Times, serif;" href="about.html">About Us</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" style="font-family: 'Times New Roman', Times, serif;"  href="contact.html">Contact Us</a>
-            </li>
-
+  <nav class="navbar navbar-expand-lg">
+    <div class="container">
+      <a class="navbar-brand" href="index.html"><h2 style="font-family: 'Times New Roman', Times, serif;">JACK <em>Pets</em></h2></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" style="font-family: 'Times New Roman', Times, serif;"  href="index.html">Home
+              <span class="sr-only">(current)</span>
+            </a>
+          </li> 
           <li class="nav-item">
-            @if (Route::has('login'))
- 
-                @auth
-                    
-                      <x-app-layout>
- 
-                      </x-app-layout></a>
-                 
-                @else
-                <li><a class="nav-link" style="font-family: 'Times New Roman', Times, serif;"  href="{{ route('login') }}" >Log in</a></li>
+            <a class="nav-link" style="font-family: 'Times New Roman', Times, serif;" href="products.html">Our Products</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" style="font-family: 'Times New Roman', Times, serif;" href="about.html">About Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" style="font-family: 'Times New Roman', Times, serif;"  href="contact.html">Contact Us</a>
+          </li>
 
-                    @if (Route::has('register'))
-                    <li> <a class="nav-link" style="font-family: 'Times New Roman', Times, serif;"  href="{{ route('register') }}">Register</a></li>
-                    @endif
-                @endauth
-      
-        @endif
-      </li>
+        <li class="nav-item">
+          @if (Route::has('login'))
 
-          </ul>
-        </div>
+              @auth
+                  
+                    <x-app-layout>
+
+                    </x-app-layout></a>
+               
+              @else
+              <li><a class="nav-link" style="font-family: 'Times New Roman', Times, serif;"  href="{{ route('login') }}" >Log in</a></li>
+
+                  @if (Route::has('register'))
+                  <li> <a class="nav-link" style="font-family: 'Times New Roman', Times, serif;"  href="{{ route('register') }}">Register</a></li>
+                  @endif
+              @endauth
+    
+      @endif
+    </li>
+
+        </ul>
       </div>
-    </nav>
-  </header>
+    </div>
+  </nav>
+</header>
