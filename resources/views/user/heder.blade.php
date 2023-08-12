fb26687be262087dadfec7397a6b240dadaf2eb4
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-12 09:05:57 +0530

    Update heder.blade.php

--- resources/views/user/heder.blade.php
+++ resources/views/user/heder.blade.php
@@ -2,25 +2,25 @@
  <header class="">
     <nav class="navbar navbar-expand-lg">
       <div class="container">
-        <a class="navbar-brand" href="index.html"><h2>JACK <em>Pets</em></h2></a>
+        <a class="navbar-brand" href="index.html"><h2 style="font-family: 'Times New Roman', Times, serif;">JACK <em>Pets</em></h2></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item active">
-              <a class="nav-link" href="index.html">Home
+              <a class="nav-link" style="font-family: 'Times New Roman', Times, serif;"  href="index.html">Home
                 <span class="sr-only">(current)</span>
               </a>
             </li> 
             <li class="nav-item">
-              <a class="nav-link" href="products.html">Our Products</a>
+              <a class="nav-link" style="font-family: 'Times New Roman', Times, serif;" href="products.html">Our Products</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="about.html">About Us</a>
+              <a class="nav-link" style="font-family: 'Times New Roman', Times, serif;" href="about.html">About Us</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="contact.html">Contact Us</a>
+              <a class="nav-link" style="font-family: 'Times New Roman', Times, serif;"  href="contact.html">Contact Us</a>
             </li>
 
           <li class="nav-item">
@@ -33,10 +33,10 @@
                       </x-app-layout></a>
                  
                 @else
-                <li><a class="nav-link" href="{{ route('login') }}" >Log in</a></li>
+                <li><a class="nav-link" style="font-family: 'Times New Roman', Times, serif;"  href="{{ route('login') }}" >Log in</a></li>
 
                     @if (Route::has('register'))
-                    <li> <a class="nav-link" href="{{ route('register') }}">Register</a></li>
+                    <li> <a class="nav-link" style="font-family: 'Times New Roman', Times, serif;"  href="{{ route('register') }}">Register</a></li>
                     @endif
                 @endauth
       
