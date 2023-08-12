9068f93c7a358553221393512ffcf73405a142de
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-12 09:29:46 +0530

    Update voucher.blade.php

--- resources/views/user/voucher.blade.php
+++ resources/views/user/voucher.blade.php
@@ -1,17 +1,17 @@
 <div class="container mt-5">
     <div class="card p-4">
-        <h1 class="text-center">Pet Shop Voucher</h1>
-        <p class="text-center">Congratulations! You've received a discount voucher at our Pet Shop.</p>
+        <h1 class="text-center" id="fonyfamily">Pet Shop Voucher</h1>
+        <p class="text-center" id="fonyfamily">Congratulations! You've received a discount voucher at our Pet Shop.</p>
         <div class="row mt-4">
             <div class="col-md-6">
-                <h2 class="text-center">Voucher Code:</h2>
-                <p class="text-center font-weight-bold">PSV12345</p>
+                <h2 id="fonyfamily" class="text-center">Voucher Code:</h2>
+                <p id="fonyfamily" class="text-center font-weight-bold">PSV12345</p>
             </div>
             <div class="col-md-6">
-                <h2 class="text-center">Discount:</h2>
-                <p class="text-center font-weight-bold">20% off</p>
+                <h2 id="fonyfamily" class="text-center">Discount</h2>
+                <p id="fonyfamily" class="text-center font-weight-bold" style="color:red">20% off</p>
             </div>
         </div>
-        <p class="text-center mt-4">Valid until: August 31, 2023</p>
+        <p class="text-center mt-4" id="fonyfamily">Valid until: August 31, 2023</p>
     </div>
 </div>
