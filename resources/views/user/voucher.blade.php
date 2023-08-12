8a37ae1a587b7ee172348c217ecafdd4bf08d43a
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-12 09:16:29 +0530

    Create voucher.blade.php

---
+++ resources/views/user/voucher.blade.php
@@ -0,0 +1,19 @@
+<div class="call-to-action">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="inner-content">
+            <div class="row">
+              <div class="col-md-8">
+                <h4>Creative &amp; Unique <em>Sixteen</em> Products</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque corporis amet elite author nulla.</p>
+              </div>
+              <div class="col-md-4">
+                <a href="#" class="filled-button">Purchase Now</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
