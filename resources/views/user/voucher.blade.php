ee068efeb150895bad88d53710db06fcd9baf4b7
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-12 09:22:12 +0530

    Update voucher.blade.php

--- resources/views/user/voucher.blade.php
+++ resources/views/user/voucher.blade.php
@@ -1,19 +1,20 @@
 <div class="call-to-action">
     <div class="container">
-      <div class="row">
-        <div class="col-md-12">
-          <div class="inner-content">
-            <div class="row">
-              <div class="col-md-8">
-                <h4>Creative &amp; Unique <em>Sixteen</em> Products</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque corporis amet elite author nulla.</p>
-              </div>
-              <div class="col-md-4">
-                <a href="#" class="filled-button">Purchase Now</a>
-              </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="inner-content">
+                    <div class="row">
+                        <div class="col-md-8">
+                            <h4>Exclusive Pet Shop Voucher</h4>
+                            <p>Discover a world of happiness for your furry friends at our pet shop. From premium pet foods to adorable toys, we have everything your pet needs.</p>
+                        </div>
+                        <div class="col-md-4">
+                            <a href="#" class="filled-button">Grab Your Voucher</a>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
     </div>
-  </div>
+</div>
+
