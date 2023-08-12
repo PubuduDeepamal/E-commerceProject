86b039c462d92b374d4cbb959701ee600d7e18d6
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-12 09:59:35 +0530

    Update voucher.blade.php

--- resources/views/user/voucher.blade.php
+++ resources/views/user/voucher.blade.php
@@ -1,10 +1,10 @@
 <div class="container mt-5">
-    <div class="card p-4" style="background-color: #f7f7f7;">
-        <h1 class="text-center" id="fonyfamily">Pet Shop Voucher</h1>
-        <p class="text-center" id="fonyfamily">Congratulations! You've received a discount voucher at our Pet Shop.</p>
+    <div class="card p-4" style="background-color: #f7f7f7;" id="boxborder">
+        <h1 class="text-center vtitle" id="fonyfamily">Pet Shop Voucher</h1>
+        <p class="text-center" id="fonyfamily">(Congratulations! You've received a discount voucher at our Pet Shop.)</p>
         <div class="row mt-4">
             <div class="col-md-6">
-                <h2 id="fonyfamily" class="text-center">Voucher Code:</h2>
+                <h2 id="fonyfamily" class="text-center">Voucher Code</h2>
                 <p id="fonyfamily" class="text-center font-weight-bold">PSV12345</p>
             </div>
             <div class="col-md-6">
