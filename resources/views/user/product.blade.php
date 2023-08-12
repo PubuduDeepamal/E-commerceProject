6ca87fe3ab171b3ac13fc29bfe60ee0006a382fb
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-12 12:39:12 +0530

    Update product.blade.php

--- resources/views/user/product.blade.php
+++ resources/views/user/product.blade.php
@@ -24,14 +24,24 @@
         @foreach ($data as $product)
 
         <div class="col-md-4">
-          <div class="product-item" style="height: 560px">
+          <div class="product-item">
             <a href="#"><img height="300" width="150" src="/productimage/{{$product->image}}" alt=""></a>
             <div class="down-content">
               <a href="#"><h4 id="fonyfamily">{{ $product->title }}</h4></a>
               <h6 id="fonyfamily">Rs {{ $product->price }}</h6>
               <p id="fonyfamily">{{ $product->description }}</p>
 
-              <a class="btn btn-primary" id="fonyfamily" href="">Add Cart</a>
+              <form action="{{ url('addcart',$product->id) }}" method="POST">
+
+                @csrf
+
+                <input type="number" value="1" min="1" class="form-control" style="width: 100px" name="quantity">
+
+                <br>
+
+                <input class="btn btn-primary" type="submit" value="Add Cart">
+              
+              </form>
               
             </div>
           </div>
