1464d5eaf92af77fde366c3fcfe77e1dd3828ece
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-12 14:20:28 +0530

    Update product.blade.php

--- resources/views/user/product.blade.php
+++ resources/views/user/product.blade.php
@@ -1,15 +1,3 @@
-@if(session()->has('message'))
-
-        <div class="alert alert-success">
-
-          {{session()->get('message')}}
-
-        <button type="button" class="close" data-dismiss="alert"> X </button>
-
-        </div>
-
-        @endif
-
 
 <div class="latest-products">
     <div class="container">
