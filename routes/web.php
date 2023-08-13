8bc44b3e069d0ccb8b8727d04ba1b0c61b17f509
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-13 09:36:11 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,17 +6,6 @@ use App\Http\Controllers\HomeController;
 
 use App\Http\Controllers\AdminController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 // Route::get('/', function () {
 //     return view('welcome');
 // });
