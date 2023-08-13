9d17da3a517c4dcbcafd6490d2768f0e6320f7c5
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-13 06:28:48 +0530

    Update voucher.blade.php

--- resources/views/user/voucher.blade.php
+++ resources/views/user/voucher.blade.php
@@ -41,7 +41,7 @@ h2{
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque corporis amet elite author nulla.</p>
                           </div>
                           <div class="col-md-4">
-                            <a href="{{ route('login') }}"  class="filled-button" style="font-family: 'Times New Roman', Times, serif;">Log in</a>
+                            <a href="{{ route('login') }}"  class="filled-button" style="font-family: 'Times New Roman', Times, serif; width:150px; text-align: center;">Log in</a>
                           </div>
                         </div>
                       </div>
