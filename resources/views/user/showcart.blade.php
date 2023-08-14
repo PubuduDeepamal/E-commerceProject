18632ed415a948484325a9aa5bf368670a83b821
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-15 03:23:19 +0530

    Update showcart.blade.php

--- resources/views/user/showcart.blade.php
+++ resources/views/user/showcart.blade.php
@@ -54,6 +54,9 @@
         <div class="container mt-5">
             <h1 class="mb-4" style="font-family: 'Times New Roman', Times, serif; font-size:38px;font-weight:bold">Product List</h1>
             <div class="table-responsive">
+
+            <form>
+
                 <table class="table table-striped table-bordered" style="text-align: center">
 
                         <tr>
@@ -78,6 +81,11 @@
                         @endforeach
 
                 </table>
+
+                <button class="btn btn-success">Confirme Order</button>
+
+            </form>
+
             </div>
         </div>
 
