583f68f3e30ed589b14f70d1ed2f146b4883bef3
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-14 05:33:02 +0530

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -16,6 +17,8 @@
 
         <!-- Styles -->
         @livewireStyles
+
+        
     </head>
     <body class="font-sans antialiased">
         <x-banner />
