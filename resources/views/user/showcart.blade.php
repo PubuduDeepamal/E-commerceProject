920b217b9ef1eb926655bee07c4cfe4685a799f2
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-15 04:23:26 +0530

    Update showcart.blade.php

--- resources/views/user/showcart.blade.php
+++ resources/views/user/showcart.blade.php
@@ -10,6 +10,7 @@
     <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
 
     <title>JackPets.lk</title>
+    <link rel="icon" type="image/png" href="assets/css/JACKPETS.png">
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
