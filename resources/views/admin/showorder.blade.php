ce0f080c06771c16ee1a663a9cb93eeb2ab6e137
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-15 09:42:00 +0530

    Update showorder.blade.php

--- resources/views/admin/showorder.blade.php
+++ resources/views/admin/showorder.blade.php
@@ -6,6 +6,33 @@
   <body>
       @include('admin.sidebar')
       @include('admin.navbar')
+
+
+      <div class="container-fluid page-body-wrapper">
+        <div class="container" align="center">
+
+
+            <table class="table table-striped table-bordered" style="text-align: center">
+
+                <tr>
+                    <th style="font-family: 'Times New Roman', Times, serif;">Customer Name</th>
+                    <th style="font-family: 'Times New Roman', Times, serif;">Phone Number</th>
+                    <th style="font-family: 'Times New Roman', Times, serif;">Address</th>
+                    <th style="font-family: 'Times New Roman', Times, serif;">Product title</th>
+                    <th style="font-family: 'Times New Roman', Times, serif;">Price</th>
+                    <th style="font-family: 'Times New Roman', Times, serif;">Quantity</th>
+                    <th style="font-family: 'Times New Roman', Times, serif;">Status</th>
+                </tr>
+
+
+
+
+        </table>
+
+        </div>
+      </div>
+
+
       @include('admin.script')
   </body>
 </html>
