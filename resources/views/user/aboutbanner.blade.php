6d3233ddfc24eb89a6eab286fad61786d0851174
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-16 04:48:22 +0530

    Update aboutbanner.blade.php

--- resources/views/user/aboutbanner.blade.php
+++ resources/views/user/aboutbanner.blade.php
@@ -3,8 +3,8 @@
       <div class="row">
         <div class="col-md-12">
           <div class="text-content">
-            <h4>new arrivals</h4>
-            <h2>Pet Products</h2>
+            <h4 style="font-family: 'Times New Roman', Times, serif;">new arrivals</h4>
+            <h2 style="font-family: 'Times New Roman', Times, serif;">Pet Products</h2>
           </div>
         </div>
       </div>
