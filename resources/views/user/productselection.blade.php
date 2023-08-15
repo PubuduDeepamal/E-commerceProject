c049955df71a0884f4d2a7a0e41488a7797ade3a
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-16 04:36:51 +0530

    Update productselection.blade.php

--- resources/views/user/productselection.blade.php
+++ resources/views/user/productselection.blade.php
@@ -4,10 +4,10 @@
         <div class="col-md-12">
           <div class="filters">
             <ul>
-                <li class="active" data-filter="*">All Products</li>
-                <li data-filter=".des">Dog Products</li>
-                <li data-filter=".dev">Cat Products</li>
-                <li data-filter=".gra">Birds Products</li>
+                <li style="font-family: 'Times New Roman', Times, serif;" class="active" data-filter="*">All Products</li>
+                <li style="font-family: 'Times New Roman', Times, serif;" data-filter=".des">Dog Products</li>
+                <li style="font-family: 'Times New Roman', Times, serif;" data-filter=".dev">Cat Products</li>
+                <li style="font-family: 'Times New Roman', Times, serif;" data-filter=".gra">Birds Products</li>
             </ul>
           </div>
         </div>
