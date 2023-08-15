b750d4743240929f700d48eeff99edbe626633b1
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-08-16 04:47:46 +0530

    Update aboutbanner.blade.php

--- resources/views/user/aboutbanner.blade.php
+++ resources/views/user/aboutbanner.blade.php
@@ -4,7 +4,7 @@
         <div class="col-md-12">
           <div class="text-content">
             <h4>new arrivals</h4>
-            <h2>sixteen products</h2>
+            <h2>Pet Products</h2>
           </div>
         </div>
       </div>
