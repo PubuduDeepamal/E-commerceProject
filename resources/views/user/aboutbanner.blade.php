08f0bf41424b740b971b095020433b1ae0da4b05
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-09-13 10:41:38 +0530

    Create aboutbanner.blade.php

---
+++ resources/views/user/aboutbanner.blade.php
@@ -0,0 +1,12 @@
+<div class="page-heading products-heading header-text">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="text-content">
+            <h2 style="font-family: 'Times New Roman', Times, serif;">About Us</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
