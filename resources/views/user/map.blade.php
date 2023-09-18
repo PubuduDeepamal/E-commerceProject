ff40b321755267efaa24ef1bdcd6198bc05f9b8b
Pubudu Deepamal <107943257+PubuduDeepamal@users.noreply.github.com>
2023-09-18 10:17:59 +0530

    Create map.blade.php

---
+++ resources/views/user/map.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Website</title>
+</head>
+<body>
+    <!-- Your website content goes here -->
+
+    <!-- Paste the Google Maps embed code here -->
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12345.6789!2d-123.456789!3d12.3456789!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTPCsDA1JzI5LjUiTiAxMjPCsDIzJzIxLjEiVw!5e0!3m2!1sen!2sus!4v1562825432432!5m2!1sen!2sus" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+    <!-- End of Google Maps embed code -->
+
+    <!-- More website content goes here -->
+</body>
+</html>
